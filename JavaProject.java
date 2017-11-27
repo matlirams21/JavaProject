3da58fdebc199769ba3910b6fa59ed6850ce29d4
matlirams21 <matlirams@gmail.com>
2017-11-27 12:04:41 +0200

    I ma adding javaProject.class and javaProject.java files to repository

---
+++ JavaProject.java
@@ -0,0 +1,10 @@
+public class javaProject
+{
+   public static void main (String [] args)
+    {
+       for(int m=1; m<=10; m++)
+        {
+          System.out.print(" " + m);
+        }
+    }
+ }
